d4230dd4c7f2f260dd7d54ef02b2ae25c0acad15
nikolashchuk <110825606+nikolashchuk@users.noreply.github.com>
2023-01-04 13:02:34 +0000

    Deploying to gh-pages from @ nikolashchuk/goit-js-hw-09@877ac3dadeec8905ced5e6a34a3373542a9d2b57 🚀

---
+++ 01-color-switcher.9cb9675f.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=null;t.addEventListener("click",(function(e){n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.9cb9675f.js.map
